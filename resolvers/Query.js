7584acb9a56ad53b61dd8f1a0c6025c113d3b260
Maciej Ząbek <maciek@maciek.local>
2019-04-21 16:42:10 +0200

    searching in string + sort

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -3,16 +3,21 @@ const totalArticles = (parent, args, { db }) =>
         .estimatedDocumentCount()
 
 const allArticles = (parent, args, { db }) => {
-    const { filter, paging } = args,
+    const { filter, paging, sorting } = args,
         createdFilterStart = filter.createdBetween && filter.createdBetween.start ? filter.createdBetween.start : new Date('1/1/1'),
-        createdFilterEnd = filter.createdBetween && filter.createdBetween.end ? filter.createdBetween.end : new Date('1/1/9999');
+        createdFilterEnd = filter.createdBetween && filter.createdBetween.end ? filter.createdBetween.end : new Date('1/1/9999'),
+        searchText = filter.searchText;
+
+    var sort = {};
+    sort[sorting.sortBy] = -1;
+
     let result;
 
     /**
      * If there are category filters look for them
      * if not, do not apply category filter
      */
-    if (filter && filter.category.length > 0) {
+    if (filter && filter.category.length > 0 && searchText) {
         result = db.collection('articles')
             .find({
                 categories: {
@@ -21,10 +26,29 @@ const allArticles = (parent, args, { db }) => {
                 created: {
                     $gt: createdFilterStart,
                     $lt: createdFilterEnd
+                },
+                $text: { 
+                    $search: filter.searchText
                 }
             })
             .limit(paging.first)
             .skip(paging.start)
+            .sort(sort)
+            .toArray()
+    } else if((!filter || filter.category.length == 0) && searchText ){
+        result = db.collection('articles')
+            .find({
+                created: {
+                    $gt: createdFilterStart,
+                    $lt: createdFilterEnd
+                },
+                $text: { 
+                    $search: filter.searchText
+                }
+            })
+            .limit(paging.first)
+            .skip(paging.start)
+            .sort(sort)
             .toArray()
     } else {
         result = db.collection('articles')
@@ -36,6 +60,7 @@ const allArticles = (parent, args, { db }) => {
             })
             .limit(paging.first)
             .skip(paging.start)
+            .sort(sort)
             .toArray()
     }
 
